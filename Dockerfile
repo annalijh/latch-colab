b3b63cdc544bcde7921397662e61df8b18f4d9d7
Stephen Lu <stephen@heroai.ca>
2022-07-20 20:29:17 -0400

    Update workflow code

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM 812206152185.dkr.ecr.us-west-2.amazonaws.com/latch-base:9a7d-main
+MAINTAINER "Stephen Lu"
 
 # Update env
 RUN apt-get update -y
@@ -17,21 +18,31 @@ ENV PATH="/root/miniconda3/bin:$PATH"
 ARG PATH="/root/miniconda3/bin:$PATH"
 
 # testing conda installation
+RUN conda update conda
+RUN conda install python=3.7.6
 RUN conda --version && conda clean --all --yes
-
-# bedtools
-RUN conda install -c bioconda bedtools
 
 # memesuite
 RUN conda install -c bioconda meme
 
-COPY data /root/reference
+# bedtools
+RUN conda install -c bioconda bedtools
+
+# handle local files
+COPY data/hg38.fa /root/reference/hg38.fa
+COPY data/motif.meme /root/reference/motif.meme
+
 COPY wf /root/wf
+
+# Download hg38.fa file from source
+# RUN wget -P /root/reference http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+# RUN gzip -d /root/reference/hg38.fa.gz
 
 # STOP HERE:
 # The following lines are needed to ensure your build environement works
 # correctly with latch.
 ARG tag
 ENV FLYTE_INTERNAL_IMAGE $tag
+RUN sed -i 's/latch/wf/g' flytekit.config
 RUN python3 -m pip install --upgrade latch
 WORKDIR /root
--- wf/__init__.py
+++ wf/__init__.py
@@ -5,13 +5,17 @@ Find Motifs using Meme Suite
 import subprocess
 from pathlib import Path
 
-from latch import medium_task, workflow
+from latch import large_task, workflow
 from latch.types import LatchFile, LatchDir
 
 
 # MEME find motifs
-@medium_task
-def run_motif_task(bed_file: LatchFile, genome_fasta: LatchFile, motif_lib: LatchFile) -> LatchDir:
+@large_task
+def run_ame_motif_task(
+    bed_file: LatchFile,
+    genome_fasta: str = "/root/reference/hg38.fa",
+    motif_lib: str = "/root/reference/motif.meme",
+) -> LatchDir:
 
     """
     Step 1: Extract DNA sequences from a fasta file based on feature coordinates.
@@ -22,7 +26,7 @@ def run_motif_task(bed_file: LatchFile, genome_fasta: LatchFile, motif_lib: Latc
         "bedtools",
         "getfasta",
         "-fi",
-        genome_fasta.local_path,
+        genome_fasta,
         "-bed",
         bed_file.local_path,
         "-fo",
@@ -42,10 +46,10 @@ def run_motif_task(bed_file: LatchFile, genome_fasta: LatchFile, motif_lib: Latc
 
     _MEME_motif_cmd = [
         "ame",
-        "--o",
+        "--oc",
         str(output_directory),
-        genome_fasta.local_path,
-        motif_lib.local_path,
+        str(output_fasta),
+        motif_lib,
     ]
 
     subprocess.run(_MEME_motif_cmd)
@@ -54,7 +58,7 @@ def run_motif_task(bed_file: LatchFile, genome_fasta: LatchFile, motif_lib: Latc
 
 
 @workflow
-def call_motifs(bed: LatchFile, genome: LatchFile, motif_lib: LatchFile) -> LatchDir:
+def call_ame_workflow(bed: LatchFile) -> LatchDir:
     """MEME-Motif analyzes genomic positions for enriched motifs. 
 
     MEME-Motif
@@ -62,8 +66,12 @@ def call_motifs(bed: LatchFile, genome: LatchFile, motif_lib: LatchFile) -> Latc
 
     Identifies known or user-provided motifs that are either relatively enriched in your sequences compared with control sequences, that are enriched in the first sequences in your input file, or that are enriched in sequences with small values of scores that you can specify with your input sequences
 
+    ### Tools used
+    - Robert McLeay and Timothy L. Bailey, "Motif Enrichment Analysis: A unified framework and method evaluation", BMC Bioinformatics, 11:165, 2010, doi:10.1186/1471-2105-11-165. [full text]
+    - Aaron R. Quinlan, Ira M. Hall, BEDTools: a flexible suite of utilities for comparing genomic features, Bioinformatics, Volume 26, Issue 6, 15 March 2010, Pages 841–842, https://doi.org/10.1093/bioinformatics/btq033
+
     __metadata__:
-        display_name: MEME Suite: AME motif caller
+        display_name: MEME Suite AME motif caller
         author:
             name: Stephen Lu
             email: stephen.lu@mail.mcgill.ca
@@ -79,33 +87,26 @@ def call_motifs(bed: LatchFile, genome: LatchFile, motif_lib: LatchFile) -> Latc
 
           __metadata__:
             display_name: bed
-
-        genome:
-          input genome file
-
-          __metadata__:
-            display_name: genome
-
-        motif_lib:
-          input motif library reference file
-
-          __metadata__:
-            display_name: library
-
         
     """
 
-    return run_motif_task(
-        bed=bed,
-        genome_fasta=genome,
-        motif_lib=motif_lib
+    return run_ame_motif_task(
+        bed_file=bed,
+        genome_fasta="/root/reference/hg38.fa",
+        motif_lib="/root/reference/motif.meme",
     )
 
 
 # Local Debugging
 if __name__ == "__main__":
-    bedFile = LatchFile("../data/")
-    genomeFastaFile = LatchFile("../data/")
-    motifLibFile = LatchFile("../data/")
+    # bedFile = LatchFile("/Users/stephenlu/Documents/latchbio/meme-suite-motif/data/test_file.bed")
 
-    call_motifs(bed=bedFile, genome=genomeFastaFile, motif_lib=motifLibFile)
+    bed_file = LatchFile("/Users/stephenlu/Documents/latchbio/meme-suite-motif/data/test_file.bed")
+    genome_path = "/Users/stephenlu/Documents/latchbio/meme-suite-motif/data/hg38.fa"
+    motif_path = "/Users/stephenlu/Documents/latchbio/meme-suite-motif/data/motif.meme"
+
+    run_ame_motif_task(
+        bed_file=bed_file,
+        genome_fasta=genome_path,
+        motif_lib=motif_path
+    )
